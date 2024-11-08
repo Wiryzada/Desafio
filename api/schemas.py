c750d43973c81de5fef658d6226b71b66062cce6
Wirysson <wiryssongomes@gmail.com>
2024-11-08 20:58:22 -0300

    configurando entrada e saída de dados

---
+++ api/schemas.py
@@ -0,0 +1,42 @@
+# api/schemas.py
+from ninja import Schema
+from decimal import Decimal
+from datetime import date
+
+class UsuarioSchema(Schema):
+    id: int
+    nome: str
+    email: str
+
+class UsuarioCreateSchema(Schema):
+    nome: str
+    email: str
+    senha: str
+
+class PedidoSchema(Schema):
+    id: int
+    usuario_id: int
+    total: Decimal
+    status: str
+    data_pedido: date
+
+class PedidoCreateSchema(Schema):
+    usuario_id: int
+    total: Decimal
+    status: str
+    data_pedido: date
+
+class ItensPedidoSchema(Schema):
+    id: int
+    pedido_id: int
+    nome: str
+    descricao: str
+    preco: Decimal
+    categoria: str
+
+class ItensPedidoCreateSchema(Schema):
+    pedido_id: int
+    nome: str
+    descricao: str
+    preco: Decimal
+    categoria: str
